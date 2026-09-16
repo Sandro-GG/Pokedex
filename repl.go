96e44e13509897fee1481877064ce87df6bc725a
Sandro Gakharia <sandrogakharia@gmail.com>
2026-09-16 18:34:17 +0400

    Add REPL interface

--- repl.go
+++ repl.go
@@ -1,9 +1,31 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func cleanInput(text string) []string {
 	clean := strings.ToLower(text)
 
 	return strings.Fields(clean)
+}
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		if scanner.Scan() {
+			input := scanner.Text()
+			cleaned := cleanInput(input)
+			if len(cleaned) == 0 {
+				continue
+			}
+
+			fmt.Printf("Your command was: %s\n", cleaned[0])
+		}
+	}
 }
