ccbce95fb0a8ccb5917dd3ecdffa40feba420b5e
Sandro Gakharia <sandrogakharia@gmail.com>
2026-09-17 23:04:24 +0400

    feat: add caching

---
+++ internal/pokecache/cache.go
@@ -0,0 +1,62 @@
+package pokecache
+
+import (
+	"sync"
+	"time"
+)
+
+type Cache struct {
+	entries  map[string]cacheEntry
+	mu       sync.RWMutex
+	interval time.Duration
+}
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+func NewCache(interval time.Duration) *Cache {
+	c := &Cache{
+		entries:  make(map[string]cacheEntry),
+		interval: interval,
+	}
+	go c.reapLoop()
+	return c
+}
+
+func (c *Cache) Add(key string, val []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.entries[key] = cacheEntry{
+		createdAt: time.Now(),
+		val:       val,
+	}
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	if val, ok := c.entries[key]; !ok {
+		return nil, false
+	} else {
+		return val.val, true
+	}
+}
+
+func (c *Cache) reapLoop() {
+	ticker := time.NewTicker(c.interval)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		c.mu.Lock()
+		for key, entry := range c.entries {
+			if time.Since(entry.createdAt) > c.interval {
+				delete(c.entries, key)
+			}
+		}
+		c.mu.Unlock()
+	}
+}
