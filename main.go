ead9ef57d9617900947638ed9e4de795f77d94a6
Sandro Gakharia <sandrogakharia@gmail.com>
2026-09-17 15:50:54 +0400

    Add map command to display locations areas

--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 type config struct {
 	commands map[string]cliCommand
+	nextURL  *string
+	prevURL  *string
 }
 
 func main() {
+	startingURL := "https://pokeapi.co/api/v2/location-area/"
+
 	cfg := config{
 		commands: map[string]cliCommand{
 			"exit": {
@@ -17,7 +21,14 @@ func main() {
 				description: "Displays a help message",
 				callback:    commandHelp,
 			},
+			"map": {
+				name:        "map",
+				description: "Displays the names of 20 location areas in the Pokemon world",
+				callback:    commandMap,
+			},
 		},
+		nextURL: &startingURL,
+		prevURL: nil,
 	}
 
 	startRepl(&cfg)
--- repl.go
+++ repl.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"strings"
 )
@@ -11,6 +14,14 @@ type cliCommand struct {
 	name        string
 	description string
 	callback    func(*config) error
+}
+
+type LocationArea struct {
+	Next     string `json:"next"`
+	Previous string `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+	} `json:"results"`
 }
 
 func cleanInput(text string) []string {
@@ -36,27 +47,63 @@ func commandHelp(cfg *config) error {
 	return nil
 }
 
+func commandMap(cfg *config) error {
+	req, err := http.NewRequest("GET", *cfg.nextURL, nil)
+	if err != nil {
+		return fmt.Errorf("error: %w", err)
+	}
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("error: %w", err)
+	}
+	defer res.Body.Close()
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return fmt.Errorf("error: %w", err)
+	}
+
+	var locs LocationArea
+	if err = json.Unmarshal(data, &locs); err != nil {
+		return fmt.Errorf("error: %w", err)
+	}
+
+	for _, loc := range locs.Results {
+		fmt.Println(loc.Name)
+	}
+
+	cfg.prevURL = cfg.nextURL
+	cfg.nextURL = &locs.Next
+
+	return nil
+}
+
 func startRepl(cfg *config) {
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for {
 		fmt.Print("Pokedex > ")
-
-		if scanner.Scan() {
-			input := scanner.Text()
-			clean := cleanInput(input)
-			if len(clean) == 0 {
-				continue
-			}
-
-			if command, ok := cfg.commands[clean[0]]; !ok {
-				fmt.Printf("Unknown command\n")
-			} else {
-				if err := command.callback(cfg); err != nil {
-					fmt.Println(err)
-				}
-			}
-
+		if !scanner.Scan() {
+			break
 		}
+		input := scanner.Text()
+		clean := cleanInput(input)
+		if len(clean) == 0 {
+			continue
+		}
+
+		if command, ok := cfg.commands[clean[0]]; !ok {
+			fmt.Printf("Unknown command\n")
+		} else {
+			if err := command.callback(cfg); err != nil {
+				fmt.Println(err)
+			}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
 	}
 }
